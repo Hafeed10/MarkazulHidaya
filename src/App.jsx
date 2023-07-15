f5268f7ce0b664725c22e9c1084cf7ec65abe99c
Sw_abah <ahmedswabah922@gmail.com>
2023-07-15 13:50:00 -0400

    modified status and others pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ function App() {
   return (
     <>
       <Home/>
-      {/* <About/> */}
-      {/* <Academics/> */}
+      <About/>
+      <Academics/>
       <Status/>
     </>
   )
--- src/Components/Status.jsx
+++ src/Components/Status.jsx
@@ -37,8 +37,8 @@ function Status() {
         }
     ]
   return (
-    <div className='w-full mt-5 md:mt-16 bg-white py-7 md:py-12 lg:py-14 h-auto  px-2 md:px-7 lg:px-20 '>
-      <div className='w-full h-full grid grid-cols-2 gap-y-7 md:grid-cols-4 text-[#35566f] items-center justify-center '>
+    <div className='w-full px-3 md:px-10 lg:px-10 xl:px-36 py-10 bg-white md:py-12 lg:py-14 h-auto'>
+      <div className='w-full h-full grid grid-cols-2 gap-y-4 md:grid-cols-4 items-center justify-center md:justify-between  '>
         {countitems.map((item)=>(
         <Items count={<span id={item.id}/>} img={item.img} measurement={item.measurement} name={item.name}/>
         ))}
@@ -53,16 +53,16 @@ export default Status
 
 function Items(props) {
   return (
-    <div className='flex flex-col px-3 md:px-10 lg:px-10 xl:px-36 py-10 bg-white justify-center space-y-7 w-full h-full items-center  '>
-        <div className='bg-[#F5DBA7] w-24 h-24 flex items-center justify-center rounded-full p-5'>
+    <div className='flex flex-col hover:shadow-md transition-all justify-center p-4 md:p-8 space-y-7 w-full h-full items-center  '>
+        <div className='bg-[#F5DBA7] w-20 h-20 md:w-24 md:h-24 flex items-center justify-center rounded-full p-4 md:p-5'>
             <img className='w-full h-full' src={props.img} alt="" />
         </div>
         <div className='font-sans flex flex-col justify-center  items-center text-center w-auto'>
-            <div className='font-semibold   text-4xl md:text-4xl flex items-center'>
+            <div className='font-semibold   text-2xl md:text-4xl flex items-center'>
                 <h2 className='text-[#555555] '>{props.count}</h2> 
                 <h2 className='text-[#555555] '>{props.measurement}</h2>
             </div>
-            <p className='font-normal tracking-wide text-gary-700  lg:text-2xl '>{props.name}</p>
+            <p className='font-normal tracking-wide text-gary-700 text-xl lg:text-2xl '>{props.name}</p>
         </div>
     </div>
   )
